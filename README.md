f15f80f286ef6a3ffa23372cf88e83e34186db26
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-03-08 09:35:18 -0600

    added a clear day button

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Day-organizer
+
+## Description
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+### Usage
+    Able to save a 24 hour work day schedule.
+<img src= "images\_C__Users_Brian_Desktop_Day-Planner_Day-organizer_index.html(Pixel 2 XL) (1).png" width="190">
+
+#### Links
+[Github profile](https://github.com/BrianSales)
+[Deployed Webpage] ()
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-    // Saving to local storage
-    // Save it 
+    //lets user see saved events
 let events = JSON.parse(localStorage.getItem('events'));
 $(document).ready(function () {
     var hour = moment().hours
@@ -9,8 +8,8 @@ $(document).ready(function () {
         events = [];
     }
     console.log(events);
-    //for loop that looks at the saved event and recalls it to the page
-    //when it is refrshed. 
+    //loop that looks at the saved event and recalls it to the page
+    //when it is refreshed. 
     for (let i = 0; i < events.length; i++) {
         const event = events[i];
         console.log('Event: ', event)
@@ -19,7 +18,7 @@ $(document).ready(function () {
     }
 
 });
-//Function allowing for date to be visible on the page. 
+//allows for date to be visible on the page. 
 function getDate() {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
@@ -28,10 +27,13 @@ getDate()
 setInterval(getDate, 1000)
 
 
+
+
+
 //function allowing for the assinged event to be saved on click 
-// I also need to grap the input's value
-// I do that by using a jquery event so that when someone click's the save button
-// I'm asking the computer to find the text that is a sibiling to the save button 
+// I also need to grab the input's value
+// I do that by using jquery so that when a user click's the save button
+// I'm asking the computer to save the user input text that is a sibiling to the save button
 $('.save').on('click', function () {
     
     const event = $(this).siblings('.event-input').children('.input').val();
@@ -43,12 +45,12 @@ $('.save').on('click', function () {
         event: event
     }
 
-    //function that returns the event and time
+    //returns saved the event and time
     const index = events.findIndex(function (event) {
         return event.time == time;
     })
 
-    //function that allows you to update your calendar if you want to remove 
+    //user needs to update your calendar if you want to remove 
     // an already saved event or add more to it
 
     if (index > -1) {
@@ -65,6 +67,16 @@ $('.save').on('click', function () {
     localStorage.setItem('events', JSON.stringify(events))
 
 })
+
+//click function that allows user to clear input of all fields on the page
+
+$("#clear-page").on("click", function(){
+    localStorage.clear();
+    window.location.reload();
+    initPage();
+
+
+  }); 
 
 var planner = document.getElementById("planner")
 
